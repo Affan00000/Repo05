29c727e5efa516b191f07fcc27c83d9b074a2e5f
Affan00000 <90653323+Affan00000@users.noreply.github.com>
2021-09-18 01:24:11 +0200

    Add files via upload

---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+
+    var lastName = "Doe";
+    var firstLetterOfLastName = " ";
+    var firstLetter = lastName[0];
+    firstLetterOfLastName += firstLetter;
+
+    // Only change code above this line
+    
+    return firstLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex14/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    // Only change code below this line
+
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks ="My " + myNoun+ " that is " + myAdjective +" "+ myVerb + " very " + myAdverb + ".";
+    
+
+    // Only change code above this line
+    
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
